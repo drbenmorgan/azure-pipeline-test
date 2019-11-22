f2f25dd135be7bda273d8db19e7bf718765f890f
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2019-11-22 18:33:46 +0000

    Basic fs check/build

--- cppfs-test.cpp
+++ cppfs-test.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
 
+#if CPPFS_IS_BOOSTFS
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
+#else
 #if __has_include(<filesystem>)
-  #include <filesystem>
+#include <filesystem>
 namespace fs = std::filesystem;
 #elif __has_include(<experimental/filesystem>)
-  #include <experimental/filesystem>
+#include <experimental/filesystem>
 namespace fs = std::experimental::filesystem;
 #endif
+#endif
 
-#if defined(__cpp_lib_filesystem) || defined(__cpp_lib_experimental_filesystem)
 void test_fs(const char* argv0) {
   fs::path p{argv0};
   std::clog << "raw path:       " << p << std::endl;
   std::clog << "canonical path: " << fs::canonical(p) << std::endl;
 }
-#endif
 
 int main(int argc, const char *argv[])
 {
 #if defined(__cpp_lib_filesystem)
   std::cout << "__cpp_lib_filesystem!" << std::endl;
-  test_fs(argv[0]);
 #elif defined(__cpp_lib_experimental_filesystem)
   std::cout << "__cpp_lib_experimental_filesystem!" << std::endl;
-  test_fs(argv[0]);
+#elif defined(CPPFS_IS_BOOSTFS)
+  std::cout << "boost::filesystem" << std::endl;
 #else
   std::cerr << "no filesystem" << std::endl;
 #endif
+  test_fs(argv[0]);
   return 0;
 }
 
